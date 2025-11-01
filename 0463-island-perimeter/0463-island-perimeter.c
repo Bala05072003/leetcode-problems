7761268f1a6204501aadb5a79c29e3d08f28ea58
Balasubramaniam <bala.23m0405@gmail.com>
2025-11-01 10:39:57 +0530

    Time: 2 ms (68.44%), Space: 14.7 MB (12.29%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.c
@@ -0,0 +1,25 @@
+int islandPerimeter(int** grid, int gridSize, int* gridColSize) {
+    int count=0;
+    int n=gridSize,m=*gridColSize;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(!grid[i][j]){
+                continue;
+            }
+            if(j+1==m ||grid[i][j+1]==0){
+                count++;
+            }
+            if(j-1<0 ||grid[i][j-1]==0){
+                count++;
+            }
+            if(i+1==n || grid[i+1][j]==0){
+                count++;
+            }
+            if(i-1<0 || grid[i-1][j]==0){
+                count++;
+            }
+           
+           }
+        }
+return count;
+}
