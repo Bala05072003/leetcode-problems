b7fc08a61d02665ed48c41e7e891f85103014c54
Balasubramaniam <bala.23m0405@gmail.com>
2026-05-03 17:49:51 +0530

    Time: 1 ms (100%), Space: 48.3 MB (98.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,24 +1,57 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length+nums2.length;
-        
-        int[] res=new int[n];
-        for(int i=0;i<nums1.length;i++){
-            res[i]=nums1[i];
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int total = n1 + n2;
+        int left = -1;
+        int right = -1;
+        if(total%2 == 0){
+            right = total/2;
+            left = total/2 - 1;
+        }else{
+            right = total/2;
         }
-        for(int i=0;i<nums2.length;i++){
-            res[nums1.length+i]=nums2[i];
-        }
-        Arrays.sort(res);
-        double median=0;
-        if(res.length%2==0){
-            median=(res[n/2])+res[(n/2)-1];
-            median=median/2;
+        int i=0; 
+        int j=0;
+        int counter = -1;
+        int median = 0;
+        while(i<n1 && j<n2){
+            int temp = 0;
+            if(nums1[i] < nums2[j]){
+                temp = nums1[i];
+                i++;
+            }else{
+                temp = nums2[j];
+                j++;
+            }
+            counter++;
+            if(counter == left || counter == right){
+                median = median + temp;
+            }
 
+            if(counter >= right){
+                break;
+            }
         }
-        else{
-            median=res[(n-1)/2];
+
+        while(i < n1){
+            counter++;
+            if(counter == left || counter == right){
+                median = median + nums1[i];
+            }
+            i++;
         }
-    return median;
+
+        while(j < n2){
+            counter++;
+            if(counter == left || counter == right){
+                median = median + nums2[j];
+            }
+            j++;
+        }
+
+        if(left == -1)
+            return median;
+        return median/2.0;
     }
 }
